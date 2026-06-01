453e7e010aafdd21c30201cf67f1f5b22c6ade93
M-Ali007 <mustafa.murta08@gmail.com>
2026-05-31 23:25:06 -0600

    imporved score system - added high score and imorved game over scren

--- Ali_Mustafa - FlappyBird.py
+++ Ali_Mustafa - FlappyBird.py
@@ -62,10 +62,24 @@ def bird_animation():
     new_bird_rect = new_bird.get_rect(center = (100, bird_rect.centery))
     return new_bird, new_bird_rect
 
-def score_display():
-    score_surface = game_font.render(str(int(score)), True, (255,255,255))
-    score_rect = score_surface.get_rect(center = (288, 100))
-    screen.blit(score_surface, score_rect)
+def score_display(game_state):
+    if game_state == "main_game":
+        score_surface = game_font.render(str(int(score)), True, (255,255,255))
+        score_rect = score_surface.get_rect(center = (288, 100))
+        screen.blit(score_surface, score_rect)
+    if game_state == "game_over":
+        score_surface = game_font.render(f"Score: {int(score)}", True, (255,255,255))
+        score_rect = score_surface.get_rect(center = (288, 100))
+        screen.blit(score_surface, score_rect)
+
+        high_score_surface = game_font.render(f"High Score: {int(highscore)}", True, (255,255,255))
+        high_score_rect = high_score_surface.get_rect(center = (288, 850))
+        screen.blit(high_score_surface, high_score_rect)
+
+def update_score(score, highscore):
+    if score > highscore:
+        highscore = score
+    return highscore
 
 pygame.init()
 screen = pygame.display.set_mode((576,1024))
@@ -80,6 +94,9 @@ bird_movement = 0
 game_active = True
 score = 0
 highscore = 0
+
+game_over_surface = pygame.transform.scale2x(pygame.image.load("assets/message.png").convert_alpha())
+game_over_rect = game_over_surface.get_rect(center = (288, 512))
 
 
 bg_surface = pygame.image.load('assets/background-day.png').convert()
@@ -126,6 +143,7 @@ while True:
                 pipe_list.clear()
                 bird_rect.center = (100, 512)
                 bird_movement = 0
+                score = 0
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipe())
         if event.type == BIRDFLAP:
@@ -151,7 +169,11 @@ while True:
 
         #score
         score += 0.01
-        score_display()
+        score_display("main_game")
+    else:
+        screen.blit(game_over_surface, game_over_rect)
+        highscore = update_score(score,highscore)
+        score_display("game_over")
 
     #floor
     floor_x_pos -= 1

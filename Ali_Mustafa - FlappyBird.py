aef1eead3473d48a0b8b876bc71c6f05b1324b6e
M-Ali007 <mustafa.murta08@gmail.com>
2026-06-04 20:00:20 -0600

    changed jump height of bird

--- Ali_Mustafa - FlappyBird.py
+++ Ali_Mustafa - FlappyBird.py
@@ -137,7 +137,7 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and game_active:
                 bird_movement = 0
-                bird_movement -= 12
+                bird_movement -= 8
             if event.key == pygame.K_SPACE and not game_active:
                 game_active = True
                 pipe_list.clear()
